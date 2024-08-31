75ac5550b5610ff5580177bee90c9a0019e45b45
_unique_ <oladipupoenoch713@gmail.com>
2024-08-31 05:26:12 -0700

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -9,3 +9,12 @@ func TestHelloWorld(t *testing.T) {
 		t.Errorf("HelloWorld() = %v; want %v", result, expected)
 	}
 }
+
+func TestHelloEnoc(t *testing.T) {
+	expected := "Hello, Enoc!"
+	result := HelloEnoc()
+	
+	if result != expected {
+		t.Errorf("HelloEnoc() = %v; want %v", result, expected)
+	}
+}
