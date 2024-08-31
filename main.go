02dfd024530e0012df4a1e76cdc833b00a2c92ae
_unique_ <oladipupoenoch713@gmail.com>
2024-08-31 05:36:25 -0700

    Update main.go

--- main.go
+++ main.go
@@ -15,3 +15,22 @@ func main() {
 	fmt.Println(HelloWorld())
 	fmt.Println(HelloMary())
 }
+
+package main
+
+import "fmt"
+
+// HelloWorld returns a greeting message.
+func HelloWorld() string {
+	return "Hello, World!"
+}
+
+// HelloEnoc returns a greeting message specifically for Enoc.
+func HelloEnoc() string {
+	return "Hello, Enoc!"
+}
+
+func main() {
+	fmt.Println(HelloWorld())
+	fmt.Println(HelloEnoc())
+}
